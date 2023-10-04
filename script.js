88c5803daec7041b1e0647e6bc43cf777a17d849
Joao Roccella <roccella@gmail.com>
2023-10-04 19:35:38 +0000

    feat: cálculo da média do aluno

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-console.log('Olá, Console!');
+// Lógica de Programação
+// Problema: obter 3 notas de um aluno, calcular a média,
+// exibir a média e exibir a situação do aluno: aprovado,
+// em exame ou reprovado.
 
-document.write('Olá, Documento!');
+console.log('Início do programa');
 
-window.alert('Olá, Janela!');
+// declaração das variáveis
+const nota1 = parseInt(prompt('Digite a nota 1:'));
+const nota2 = parseInt(prompt('Digite a nota 2:'));
+const nota3 = parseInt(prompt('Digite a nota 3:'));
+
+// cálculo da média
+const media = (nota1 + nota2 + nota3) / 3;
+
+console.log('A nota 1 é:', nota1);
+console.log('A nota 2 é:', nota2);
+console.log('A nota 3 é:', nota3);
+
+console.log('A média do aluno é:', media);
+
+console.log('Fim do programa');
