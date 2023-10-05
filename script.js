d0a7c4b8060a512e886cd57b534ada78096131ff
Joao Roccella <roccella@gmail.com>
2023-10-05 16:15:20 +0000

    feat: implementação da interface na página

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// Lógica de programação
-// Problema: receber 3 notas de um aluno,
-// calcular e apresentar a média e o resultado
-// se o aluno foi aprovado ou não.
+function mediaAluno() {
 
-console.log('Início do programa');
+    // Lógica de programação
+    // Problema: receber 3 notas de um aluno,
+    // calcular e apresentar a média e o resultado
+    // se o aluno foi aprovado, reprovado ou ficou de exame.
 
-let nota1;
-let nota2;
-let nota3;
+    console.log('Início do programa');
 
-nota1 = parseInt(prompt('Digite a nota 1:'));
-nota2 = parseInt(prompt('Digite a nota 2:'));
-nota3 = parseInt(prompt('Digite a nota 3:'));
+    // declaração das "variáveis"
+    const nota1 = parseInt(document.getElementById('nota1').value);
+    const nota2 = parseInt(document.getElementById('nota2').value);
+    const nota3 = parseInt(document.getElementById('nota3').value);
 
-console.log('A nota 1 é:', nota1);
-console.log('A nota 2 é:', nota2);
-console.log('A nota 3 é:', nota3);
+    // cálculo da média
+    const media = (nota1 + nota2 + nota3) / 3;
 
-// cálculo da média
-media = (nota1 + nota2 + nota3) / 3;
+    document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media + '<br>';
 
-console.log('A média do aluno é: ', media);
+    if (media >= 7) {
+        document.getElementById('resultado').innerHTML += 'APROVADO';
+    } else if (media >= 5) {
+        document.getElementById('resultado').innerHTML += 'EXAME';
+    } else {
+        document.getElementById('resultado').innerHTML += 'REPROVADO';
+    }
 
-console.log('Fim do programa');
+    console.log('Fim do programa');
+
+}
