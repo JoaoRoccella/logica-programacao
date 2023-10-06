3f2ead4c193f84a16636ab61c0c0dbf18c5ca84e
Joao Roccella <roccella@gmail.com>
2023-10-06 11:41:23 +0000

    feat: implementando uma interface de usuário

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-// Lógica de Programação
-// Problema: obter 3 notas de um aluno, calcular a média,
-// exibir a média e exibir a situação do aluno: aprovado,
-// em exame ou reprovado.
+function mediaAluno() {
 
-console.log('Início do programa');
+    // Lógica de Programação
+    // Problema: obter 3 notas de um aluno, calcular a média,
+    // exibir a média e exibir a situação do aluno: aprovado,
+    // em exame ou reprovado.
 
-// declaração das variáveis
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'));
-const nota3 = parseInt(prompt('Digite a nota 3:'));
+    console.log('Início do programa');
 
-// cálculo da média
-const media = (nota1 + nota2 + nota3) / 3;
+    // declaração das variáveis
+    const nota1 = parseInt(document.getElementById('nota1').value);
+    const nota2 = parseInt(document.getElementById('nota2').value);
+    const nota3 = parseInt(document.getElementById('nota3').value);
 
-console.log('A nota 1 é:', nota1);
-console.log('A nota 2 é:', nota2);
-console.log('A nota 3 é:', nota3);
+    // cálculo da média
+    const media = (nota1 + nota2 + nota3) / 3;
 
-console.log('A média do aluno é:', media);
+    console.log('A nota 1 é:', nota1);
+    console.log('A nota 2 é:', nota2);
+    console.log('A nota 3 é:', nota3);
 
-console.log('Fim do programa');
+    document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media + '<br>';
+
+    if (media >= 7) {
+        document.getElementById('resultado').innerHTML += 'Situação: APROVADO';
+    } else if (media >= 5) {
+        document.getElementById('resultado').innerHTML += 'Situação: EXAME';
+    } else {
+        document.getElementById('resultado').innerHTML += 'Situação: REPROVADO';
+    }
+
+    console.log('Fim do programa');
+
+}
